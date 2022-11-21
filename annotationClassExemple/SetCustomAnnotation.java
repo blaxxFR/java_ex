fb3b8b84892cba9b14cc325d094b467bca6e7dfa
Nathan <amsellem.n@gmail.com>
2022-11-21 17:20:04 +0100

    1st

---
+++ annotationClassExemple/SetCustomAnnotation.java
@@ -0,0 +1,13 @@
+package annotationClassExemple;
+
+
+//This is the annotation being applied to a class
+@TypeHeader(developer = "Bob Bee",
+ lastModified = "2018-02-12",
+ teamMembers = { "Ann", "Dan", "Fran" },
+ meaningOfLife = 42)
+
+public class SetCustomAnnotation {
+ // Class contents go here
+	
+}
